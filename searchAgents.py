21b83f03669e85a1f77acc9191013c50864c4105
Vladlen <ilusenkovladlen@gmail.com>
2022-09-26 21:24:23 +0300

    Added CornerProblem. Probably wrong

--- searchAgents.py
+++ searchAgents.py
@@ -284,7 +284,7 @@ class CornersProblem(search.SearchProblem):
         self.walls = startingGameState.getWalls()
         self.startingPosition = startingGameState.getPacmanPosition()
         top, right = self.walls.height-2, self.walls.width-2
-        self.corners = ((1,1), (1,top), (right, 1), (right, top))
+        self.corners = ((1, 1), (1, top), (right, 1), (right, top))
         for corner in self.corners:
             if not startingGameState.hasFood(*corner):
                 print('Warning: no food in corner ' + str(corner))
@@ -292,7 +292,9 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         "*** YOUR CODE HERE ***"
-
+        self.visitedCorners = dict.fromkeys(self.corners, False)
+        if self.startingPosition in self.visitedCorners:
+            self.visitedCorners[self.startingPosition] = True
 
     def getStartState(self):
         """
@@ -300,14 +302,18 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition, self.visitedCorners
 
     def isGoalState(self, state: Any):
         """
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        visited = state[1]
+        for corner in visited:
+            if not corner:
+                return False
+        return True
 
     def getSuccessors(self, state: Any):
         """
@@ -322,14 +328,21 @@ class CornersProblem(search.SearchProblem):
 
         successors = []
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
-            # Add a successor state to the successor list if the action is legal
-            # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
+            #   x,y = state
             #   dx, dy = Actions.directionToVector(action)
             #   nextx, nexty = int(x + dx), int(y + dy)
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            x, y = state[0]
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            if not self.walls[nextx][nexty]:
+                next_position = (nextx, nexty)
+                new_visited = state[1].copy()
+                if next_position in new_visited:
+                    new_visited[next_position] = True
+                successors.append(((next_position, new_visited), action))
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
